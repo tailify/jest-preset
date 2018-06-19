65d5ef8d56ffec9d6d14d4c38b4a732972192742
Jamie Connolly <jamie.connolly@tailify.com>
2018-06-19 10:40:18 +0100

    Add in Babel support for test files

--- jest-preset.js
+++ jest-preset.js
@@ -2,10 +2,11 @@
 
 module.exports = {
   globalSetup: '@tailify/jest-preset/global-setup',
-  moduleFileExtensions: ['feature', 'js'],
+  moduleFileExtensions: ['feature', 'js', 'mjs'],
   setupFiles: ['@tailify/jest-preset/setup'],
   testMatch: ['**/features/**/*.feature'],
   transform: {
     '^.+\\.feature$': 'gherkin-jest',
+    '^.+\\.m?js$': 'babel-jest',
   },
 };
