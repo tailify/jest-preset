a54e439a70fa2a9ca1790628f6f2770bcad39531
Jamie Connolly <jamie.connolly@tailify.com>
2018-06-19 11:17:06 +0100

    Add in Enzyme support

--- jest-preset.js
+++ jest-preset.js
@@ -4,6 +4,11 @@ module.exports = {
   globalSetup: '@tailify/jest-preset/global-setup',
   moduleFileExtensions: ['feature', 'js', 'mjs'],
   setupFiles: ['@tailify/jest-preset/setup'],
+  setupTestFrameworkScriptFile: 'jest-enzyme',
+  testEnvironment: 'enzyme',
+  testEnvironmentOptions: {
+    enzymeAdapter: 'react16',
+  },
   testMatch: ['**/features/**/*.feature'],
   transform: {
     '^.+\\.feature$': 'gherkin-jest',
